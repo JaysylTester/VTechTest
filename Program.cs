b66c4d97119397d1202de16bffb63086a779b62d
EricA-G <eric.armah-godwin@hotmail.co.uk>
2017-02-07 11:15:08 +0000

    added Program.cs and SetMethods.cs files

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+
+
+
+namespace ValtechProj1
+{
+    class Program
+    {
+        //Create the reference for our browser
+        //Nb: Please ensure link to ChromeDriver is customised to suit that of devise you're running test on        
+        IWebDriver driver = new ChromeDriver("C:\\Users\\GlobalLightRevival\\chromedriver_jan");
+
+        static void Main(string[] args)
+        {           
+                        
+        }
+
+        [SetUp]
+        public void Initialize()
+        {
+            //Navigate to google page
+            driver.Navigate().GoToUrl("https://www.valtech.com/");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Assert that the “Latest News” section is displaying           
+
+            //Nb: a separate repository/library 
+            string casPath = "//a[@href='/cases/']";
+            string servPath = "//a[@href='/services/']";
+            string carPath = "//a[@href='/jobs/']";
+            string contPath = "//div[@class='hamburger__flipper']";
+            string newsSectionHeader = "//h2[@class='block-header__heading']";
+
+            //Navigate to Cases section and assert that H1 tag is displaying the page name
+            SetMethods.verifyHomePageObjects(driver, newsSectionHeader, "XPath");
+            SetMethods.navToCasesPage(driver, casPath, "XPath");
+            SetMethods.navToServicesPage(driver, servPath, "XPath");
+            SetMethods.navToCareersPage(driver, carPath, "XPath");
+            SetMethods.navigateToContactsPage(driver, contPath, "XPath");      
+        }        
+
+        
+
+        [TearDown]
+        public void CleanUp()
+        {
+            //Close the browser            
+            driver.Close();
+        }
+    }
+}
---
+++ SetMethods.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace ValtechProj1
+{
+    class SetMethods
+    {        
+        public static void navToCasesPage(IWebDriver driver, string element, string elementtype)
+        {
+            driver.FindElement(By.XPath(element)).Click();
+            var caseSection = driver.FindElement(By.XPath("//*[h1='Work']"));
+            Assert.IsTrue(caseSection.Text.Contains("Work"));            
+        }
+
+        public static void navToServicesPage(IWebDriver driver, string element, string elementtype)
+        {
+            driver.FindElement(By.XPath(element)).Click();
+            var caseSection = driver.FindElement(By.XPath("//*[h1='Services']"));
+            Assert.IsTrue(caseSection.Text.Contains("Services"));
+        }
+
+        public static void navToCareersPage(IWebDriver driver, string element, string elementtype)
+        {
+            driver.FindElement(By.XPath(element)).Click();
+            var caseSection = driver.FindElement(By.XPath("//*[h1='Careers']"));
+            Assert.IsTrue(caseSection.Text.Contains("Careers"));
+        }        
+
+        public static void navigateToContactsPage(IWebDriver driver, string element, string elementtype)
+        {
+            driver.FindElement(By.XPath(element)).Click();
+            int iCount = driver.FindElements(By.XPath("//i[@class='office__map__icon glyph']")).Count;
+            Console.WriteLine(iCount);
+        }
+
+        public static void verifyHomePageObjects(IWebDriver driver, string element, string elementtype)
+        {
+            var newsSection = driver.FindElement(By.XPath(element));
+            Assert.IsTrue(driver.PageSource.Contains("Latest news"));
+            Console.WriteLine("Latest News validated");
+        }
+    }
+}
+
